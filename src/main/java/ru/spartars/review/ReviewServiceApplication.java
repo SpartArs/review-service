9d78da98af9c791e9c05875eda754295912169a0
spartars <spartars@yandex.ru>
2019-08-06 22:19:21 +0300

    adding upload path

--- src/main/java/ru/spartars/review/ReviewServiceApplication.java
+++ src/main/java/ru/spartars/review/ReviewServiceApplication.java
@@ -3,18 +3,16 @@ package ru.spartars.review;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.spartars.review.entity.CategoryEntity;
-import ru.spartars.review.entity.CommentEntity;
-import ru.spartars.review.entity.ReviewEntity;
-import ru.spartars.review.entity.UserEntity;
-import ru.spartars.review.repository.CategoryRepository;
-import ru.spartars.review.repository.CommentRepository;
-import ru.spartars.review.repository.ReviewRepository;
-import ru.spartars.review.repository.UserRepository;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import ru.spartars.review.entity.*;
+import ru.spartars.review.repository.*;
 
+import javax.servlet.MultipartConfigElement;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -61,18 +59,27 @@ public class ReviewServiceApplication {
             userRepository.save(vasya);
             userRepository.save(petya);
 
-            var category = new CategoryEntity(
+            var category1 = new CategoryEntity(
                     0L,
-                    "Category"
+                    "Category-item"
             );
 
-            categoryRepository.save(category);
+            var category2 = new CategoryEntity(
+                    0L,
+                    "Category-it"
+            );
+
+            categoryRepository.save(category1);
+            categoryRepository.save(category2);
+
+
 
             var vasyaReview = new ReviewEntity(
                     0L,
                     "Vasya review",
                     vasya,
-                    category,
+                    category1,
+                    "https://c.dns-shop.ru/thumb/st1/fit/600/374/f67c67b370c460ff797ec657bae263e3/78a7f0df1589613b4e49a58f5adfa16fefef7cb20d70901b39c13e44af58b8de.jpg",
                     LocalDateTime.now(),
                     List.of("vasya", "tags")
             );
@@ -84,7 +91,8 @@ public class ReviewServiceApplication {
                     0L,
                     "Petya review",
                     petya,
-                    category,
+                    category2,
+                    "https://i2.rozetka.ua/goods/10292348/68512101_images_10292348175.jpg",
                     LocalDateTime.now(),
                     List.of("petya", "tags")
             ));
@@ -96,9 +104,6 @@ public class ReviewServiceApplication {
                     petya
             ));
 
-
-//      emailService.sendSimpleMessage("...", "...", "Verification message", "Link must be here");
-//      emailService.sendMimeMessage("...", "...", "Verification message", "<a href='http://localhost/verification'>Link must be here</a>");
         };
     }
 
